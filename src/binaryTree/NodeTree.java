5b5b98ef46e65bcf81c9ee77b87a0f3329bb53d8
patenda <ruanwei1205@gmail.com>
2018-09-29 19:07:35 -0700

    first commit

---
+++ src/binaryTree/NodeTree.java
@@ -0,0 +1,34 @@
+package binaryTree;
+
+public class NodeTree {
+	int data; // 根节点数据
+	NodeTree left; // 左子树
+	NodeTree right; // 右子树
+
+	public NodeTree() {
+		super();
+	}
+
+	public NodeTree(int data) { // 实例化二叉树
+		super();
+		this.data = data;
+		left = null;
+		right = null;
+	}
+
+	public void insert(NodeTree root, int data) {
+		if (data > root.data) { // 如果插入的节点大于跟节点
+			if (root.right == null) { // 如果右子树为空，就插入，如果不为空就再创建一个节点
+				root.right = new NodeTree(data); // 就把插入的节点放在右边
+			} else {
+				this.insert(root.right, data);
+			}
+		} else { // 如果插入的节点小于根节点
+			if (root.left == null) { // 如果左子树为空，就插入，如果不为空就再创建一个节点
+				root.left = new NodeTree(data); // 就把插入的节点放在左边边
+			} else {
+				this.insert(root.left, data);
+			}
+		}
+	}
+}
